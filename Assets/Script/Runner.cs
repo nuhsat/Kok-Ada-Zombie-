9867e37c81827f289a66986e2be3df73b9498114
nuhsat <nuhsatria@gmail.com>
2017-12-27 17:27:48 +0700

    kok ada zombie ?!

---
+++ Assets/Script/Runner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+	void Update () {
+		transform.Translate(0f, 0f, 5f * Time.deltaTime);
+	}
+}
